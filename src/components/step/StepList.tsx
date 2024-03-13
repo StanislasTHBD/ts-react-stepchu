a5b9132b21b182f8acee6b88c077e5c68b81fe7c
StanislasTHBD <stanislas.thiebaud.lycee@gmail.com>
2024-03-13 16:20:54 +0100

    modification step

--- src/components/step/StepList.tsx
+++ src/components/step/StepList.tsx
@@ -1,3 +1,48 @@
+// import React from 'react';
+// import Steps from '../../models/Steps';
+// import Utilisateur from '../../models/Utilisateur';
+
+// interface StepListProps {
+//   steps: Steps[];
+//   utilisateurs: Utilisateur[];
+// }
+
+// function formatDate(date: Date): string {
+//   const formattedDate = new Date(date).toISOString().split('T')[0];
+//   const [year, month, day] = formattedDate.split('-');
+//   return `${day}/${month}/${year}`;
+// }
+
+// const StepList: React.FC<StepListProps> = ({ steps, utilisateurs }) => {
+//   return (
+//     <div className="max-w-3xl mx-auto">
+//       <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+//         <div className="bg-white divide-y divide-gray-200">
+//           <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 bg-custom-blue bg-opacity-80 text-xs font-semibold text-custom-secondary uppercase tracking-wider">
+//             <div className="p-4 text-center">Date</div>
+//             <div className="p-4 text-center">Nombre de pas</div>
+//             <div className="p-4 text-center">Utilisateur</div>
+//           </div>
+//           {steps.map((step) => {
+//             const utilisateur = utilisateurs.find((user) => user.id === step.userId);
+//             const nomUtilisateur = utilisateur ? utilisateur.name : "Utilisateur inconnu";
+//             return (
+//               <div key={step.id} className="grid grid-cols-1 sm:grid-cols-3 gap-4 hover:bg-custom-orange">
+//                 <div className="p-4 text-center">{formatDate(step.date)}</div>
+//                 <div className="p-4 text-center">{step.steps}</div>
+//                 <div className="p-4 text-center">{nomUtilisateur}</div>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default StepList;
+
+
 import React from 'react';
 import Steps from '../../models/Steps';
 import Utilisateur from '../../models/Utilisateur';
@@ -18,11 +63,17 @@ const StepList: React.FC<StepListProps> = ({ steps, utilisateurs }) => {
     <div className="max-w-3xl mx-auto">
       <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
         <div className="bg-white divide-y divide-gray-200">
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 bg-custom-blue bg-opacity-80 text-xs font-semibold text-custom-secondary uppercase tracking-wider">
-            <div className="p-4 text-center">Date</div>
-            <div className="p-4 text-center">Nombre de pas</div>
-            <div className="p-4 text-center">Utilisateur</div>
-          </div>
+          {steps.length === 0 ? (
+            <div className="bg-custom-blue bg-opacity-80 shadow-md rounded-lg p-4">
+              <p className="text-lg font-semibold text-center text-custom-secondary">Aucun step</p>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 bg-custom-blue bg-opacity-80 text-xs font-semibold text-custom-secondary uppercase tracking-wider">
+              <div className="p-4 text-center">Date</div>
+              <div className="p-4 text-center">Nombre de pas</div>
+              <div className="p-4 text-center">Utilisateur</div>
+            </div>
+          )}
           {steps.map((step) => {
             const utilisateur = utilisateurs.find((user) => user.id === step.userId);
             const nomUtilisateur = utilisateur ? utilisateur.name : "Utilisateur inconnu";
