8e2012b580ea0e68556571e94bc48ea371fbba9d
StanislasTHBD <stanislas.thiebaud.lycee@gmail.com>
2024-03-13 10:27:00 +0100

    🐛 Correction password utilisateur

--- src/models/Utilisateur.tsx
+++ src/models/Utilisateur.tsx
@@ -7,7 +7,8 @@ interface Utilisateur {
     id?: string;
     name: string; // Nom générer par le chu
     // securityQuestion?: QuestionSecurity; // ID de la question de sécurité choisie par l'utilisateur
-    securityAnswer?: string; // Réponse à la question de sécurité
+    // securityAnswer?: string; // Réponse à la question de sécurité
+    password?: string; 
     phoneId?: string; // Récuperation de l'identifiant unique du téléphone de l'utilisateur
     // badges: Badge[]; // Badges obtenus par l'utilisateur
     // completedQuizz: CompletedQuizz[]; // Tableau des quizz complétés par l'utilisateur
